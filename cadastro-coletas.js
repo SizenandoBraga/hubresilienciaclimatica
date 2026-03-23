2fbc89780c63a7a3bac57ac3db8023cc5e8fa50d
Sizenando Braga <70855521+SizenandoBraga@users.noreply.github.com>
2026-03-23 19:10:22 +0000

    atualizar

--- cadastro-coletas.js
+++ cadastro-coletas.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase-init.js";
+import { auth, db } from "firebase-init.js";
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import {
