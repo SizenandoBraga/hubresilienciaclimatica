88842b0a44c89dc5372bbdffba448b1bc3975364
Sizenando Braga <70855521+SizenandoBraga@users.noreply.github.com>
2026-05-27 21:46:54 +0000

    Atualizar

--- firebase-init.js
+++ firebase-init.js
@@ -8,13 +8,13 @@ import {
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK3U8d8Ql1xoviPe-KJ6B8xvOZ7_K4vuQ",
-  authDomain: "plataforma-regenera.firebaseapp.com",
-  projectId: "plataforma-regenera",
-  storageBucket: "plataforma-regenera.firebasestorage.app",
-  messagingSenderId: "203232025444",
-  appId: "1:203232025444:web:b2b745121103e0bce7f871"
-};
+    apiKey: "AIzaSyCK3U8d8Ql1xoviPe-KJ6B8xvOZ7_K4vuQ",
+      authDomain: "plataforma-regenera.firebaseapp.com",
+        projectId: "plataforma-regenera",
+          storageBucket: "plataforma-regenera.firebasestorage.app",
+            messagingSenderId: "203232025444",
+              appId: "1:203232025444:web:b2b745121103e0bce7f871"
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
