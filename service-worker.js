092d600808979dd2c4729fb993a4da1f94b2460e
Sizenando Braga <70855521+SizenandoBraga@users.noreply.github.com>
2026-06-09 18:14:54 +0000

    Atualizar

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,13 @@
-const CACHE_NAME = "nsru-app-v1";
+const CACHE_NAME = "hub-resiliencia-v1";
 
-const APP_FILES = [
-  "./",
-  "./login.html",
-  "./cooperativa-vila-pinto.html",
-  "./cooperativa-cooadesc.html",
-  "./cooperativa-padre-cacique.html"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
