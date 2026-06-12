d76fc22da52874170477f9bf276a22f043e2473c
Sizenando Braga <70855521+SizenandoBraga@users.noreply.github.com>
2026-06-12 20:12:20 +0000

    Atualizar

--- cooperativas-territorio.js
+++ cooperativas-territorio.js
@@ -1,8 +1,7 @@
-import { auth, db } from "./firebase-init-vp.js";
-import "./polyfills.js";
-import { db as dbGuardioes } from "./firebase-init-guardioes.js";
-import { registerAccessLog } from "./access-tracker.js";
-import {onAuthStateChanged,signOut} from "firebase/auth";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 import {
   doc,
@@ -15,7 +14,7 @@ import {
   deleteDoc,
   serverTimestamp,
   getDocs
-} from "firebase/firestore";
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 /* =========================================================
    CONFIG
