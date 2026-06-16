f80e490f22e38d7f9e83de6b262bf7b4c491ed54
Sizenando Braga <70855521+SizenandoBraga@users.noreply.github.com>
2026-06-16 18:29:08 +0000

    ATUALIZAR

---
+++ firebase-init-guardioes.js
@@ -0,0 +1,26 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABDs2g2yHJBxcIq4cQNt6BNJGO__4Liks",
+  authDomain: "guardioesurbanos-a144f.firebaseapp.com",
+  projectId: "guardioesurbanos-a144f",
+  storageBucket: "guardioesurbanos-a144f.firebasestorage.app",
+  messagingSenderId: "354730831720",
+  appId: "1:354730831720:web:7e68c4ad9e61bf310770dd",
+  measurementId: "G-HS27X087L5"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+await setPersistence(auth, browserLocalPersistence);
+
+const db = getFirestore(app);
+
+export { app, auth, db };
